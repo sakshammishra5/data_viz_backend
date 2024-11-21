b803740e1d42a459440a06efd906832594df9f9c
sakshammishraGla <sakshammishra5thsep@gmail.com>
2024-11-21 18:06:53 +0530

    added vercel config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const cors = require('cors')
+require('dotenv').config()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -14,7 +15,7 @@ app.use(require('./routes/feature'))
 // app.use(require("./routes/addfeature"))
 // app.use(require("./routes/feature"))
 
-mongoose.connect('mongodb://localhost:27017/dataVisualization')
+mongoose.connect(process.env.MONGO_URL||'mongodb://localhost:27017/dataVisualization')
     .then(() => {
         app.listen(port, () => {
             console.log(`http://localhost:${port}`);
