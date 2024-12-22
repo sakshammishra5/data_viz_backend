adad32c4eb4f1b909490f0a6e9a54be9815cec86
sakshammishraGla <sakshammishra5thsep@gmail.com>
2024-12-22 09:28:24 +0530

    changes in cors origin:-Url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://data-viz-frontend.vercel.app",
     credentials: true,
 }))
 
