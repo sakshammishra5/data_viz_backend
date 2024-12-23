087cc8554823fbb72d8699e816816d1cd8ebc906
sakshammishraGla <sakshammishra5thsep@gmail.com>
2024-12-23 10:31:32 +0530

    changes

--- app.js
+++ app.js
@@ -6,34 +6,9 @@ const cors = require('cors')
 require('dotenv').config()
 const cookieParser = require('cookie-parser')
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://data-viz-frontend.vercel.app'
-];
 
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-});
+app.use(cors())
 
-app.use(cors({
-    credentials: true,
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
-}))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
